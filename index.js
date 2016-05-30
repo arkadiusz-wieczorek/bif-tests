1484f3fc59d88322eefa72f9cb80b2139ae20b3c
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-05-30 21:45:00 +0200

    Modify minimumDistance test & add multiple executions

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var generator = new MersenneTwister();
 var lcg = require ('lcg-rnd');
 lcg.srand = 10;
 
+var fs = require('fs');
+
+
 function distancePoints(a,b,dimension3D) {
     var dist = 0;
     if (!dimension3D) {
@@ -89,7 +92,7 @@ function minimumDistance(randomFunction) {
 
         if (minimum > distance) minimum = distance;
     }
-    return Math.pow(minimum, 2)
+    return 1 - Math.exp(Math.pow(minimum, 2)*(-1)/0.995)
 }
 
 // Random spheres test
@@ -115,25 +118,122 @@ function randomSpheres(randomFunction) {
 var MT = () => generator.random_incl()
 var LCG = () => lcg.random()
 
-console.log('Mersenne Twister')
-console.log('----------------')
-console.log('PI test                ', pi(2000, MT))
-console.log('Parking lot test       ', parking(MT))
-console.log('Minimum distance test  ', minimumDistance(MT))
-console.log('Random spheres test    ', randomSpheres(MT))
 
 
-console.log('\nLinear Congruential Generator')
-console.log('-----------------------------')
-console.log('PI test                ', pi(2000, LCG))
-console.log('Parking lot test       ', parking(LCG))
-console.log('Minimum distance test  ', minimumDistance(LCG))
-console.log('Random spheres test    ', randomSpheres(LCG))
+
+//
+// console.log('Mersenne Twister')
+// console.log('----------------')
+// console.log('PI test                ', pi(2000, MT))
+// console.log('Parking lot test       ', parking(MT))
+// console.log('Minimum distance test  ', minimumDistance(MT))
+// console.log('Random spheres test    ', randomSpheres(MT))
+//
+//
+// console.log('\nLinear Congruential Generator')
+// console.log('-----------------------------')
+// console.log('PI test                ', pi(2000, LCG))
+// console.log('Parking lot test       ', parking(LCG))
+// console.log('Minimum distance test  ', minimumDistance(LCG))
+// console.log('Random spheres test    ', randomSpheres(LCG))
+//
+//
+// console.log(`\nJavaScript's Math.random`)
+// console.log('------------------------')
+// console.log('PI test                ', pi(2000, Math.random))
+// console.log('Parking lot test       ', parking(Math.random))
+// console.log('Minimum distance test  ', minimumDistance(Math.random))
+// console.log('Random spheres test    ', randomSpheres(Math.random))
+
+var records = {
+    "pi": {
+        'mersenne-twister': [],
+        'mersenne-twister-average': 0,
+        'lcg': [],
+        'lcg-avarage': 0,
+        'Math.random': [],
+        'Math.random-average': 0
+    },
+    "parking":{
+        'mersenne-twister': [],
+        'mersenne-twister-average': 0,
+        'lcg': [],
+        'lcg-avarage': 0,
+        'Math.random': [],
+        'Math.random-average': 0
+    },
+    "minimum":{
+        'mersenne-twister': [],
+        'mersenne-twister-average': 0,
+        'lcg': [],
+        'lcg-avarage': 0,
+        'Math.random': [],
+        'Math.random-average': 0
+    },
+    "spheres":{
+        'mersenne-twister': [],
+        'mersenne-twister-average': 0,
+        'lcg': [],
+        'lcg-avarage': 0,
+        'Math.random': [],
+        'Math.random-average': 0
+    }
+
+}
+
+for (var i = 0; i < 100; i++) {
+    records['pi']['mersenne-twister'].push(pi(2000, MT))
+    records['pi']['lcg'].push(pi(2000, LCG))
+    records['pi']['Math.random'].push(pi(2000, Math.random))
+    if (i % 10 === 0) {
+        console.log('pi', i)
+    }
+}
+
+for (var i = 0; i < 10; i++) {
+    records['parking']['mersenne-twister'].push(parking(MT))
+    records['parking']['lcg'].push(parking(LCG))
+    records['parking']['Math.random'].push(parking(Math.random))
+    console.log('parking', i)
+}
 
 
-console.log(`\nJavaScript's Math.random`)
-console.log('------------------------')
-console.log('PI test                ', pi(2000, Math.random))
-console.log('Parking lot test       ', parking(Math.random))
-console.log('Minimum distance test  ', minimumDistance(Math.random))
-console.log('Random spheres test    ', randomSpheres(Math.random))
+for (var i = 0; i < 20; i++) {
+    records['spheres']['mersenne-twister'].push(randomSpheres(MT))
+    records['spheres']['lcg'].push(randomSpheres(LCG))
+    records['spheres']['Math.random'].push(recordsrandomSpheres(Math.random))
+    console.log('randomSpheres', i)
+}
+
+for (var i = 0; i < 100; i++) {
+    records['minimum']['mersenne-twister'].push(minimumDistance(MT))
+    records['minimum']['lcg'].push(minimumDistance(LCG))
+    records['minimum']['Math.random'].push(minimumDistance(Math.random))
+    if (i % 10 === 0) {
+        console.log('minimumDistance', i)
+    }
+}
+
+for (var key in records) {
+    for (var subkey in records[key]) {
+        var array = records[key][subkey];
+        var sum = 0;
+        if (Array.isArray(array)) {
+            for (var i = 0; i < array.length; i++) {
+                sum = sum + array[i];
+            }
+            var average_key = subkey+"-average"
+            records[key][average_key] = sum / array.length;
+        }
+    }
+}
+
+console.log(records)
+
+fs.writeFile("results.log",JSON.stringify(records), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+});
